7981ba688b56d0bf4edbc1d47b3bb476b4308e9a
schwyzz <schwyz@protonmail.ch>
2024-04-13 15:46:26 +0300

    utf-8 whis BOM

--- Source.c
+++ Source.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+п»ї#include <stdio.h>
 #include <locale.h>
 int main() {
 	setlocale(LC_ALL, "");
-	printf("  Hello Dolly!\n Максим привет как дела и как там вообще чо.\n");
+	printf("  Hello Dolly!\n РњР°РєСЃРёРј РїСЂРёРІРµС‚ РєР°Рє РґРµР»Р° Рё РєР°Рє С‚Р°Рј РІРѕРѕР±С‰Рµ С‡Рѕ.\n");
 	_getch();
-	printf(" \n Для выхода нажми кнопку\n");
+	printf(" \n Р”Р»СЏ РІС‹С…РѕРґР° РЅР°Р¶РјРё РєРЅРѕРїРєСѓ\n");
 	_getch();
 
 }
